68fd138fd673d0f42c4191bf7d03ec617513860f
Abdallah <elshenawya894@gmail.com>
2024-09-07 21:51:41 +0300

    Initial project setup with Clean Architecture

---
+++ PosSystem/PosSystem.Core/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PosSystem.Core.Interfaces
+{
+    public interface IRepository<Entity>
+    {
+        public List<Entity> GetAll();
+        public Entity GetById(string id);
+        public void Insert(Entity entity);
+        public void Update(Entity entity);
+        public void Delete(string id);
+        public void Save();
+    }
+}
---
+++ PosSystem/PosSystem/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using PosSystem.Infrastracture;
+using PosSystem.Infrastracture.Persistence.Data;
+using PosSystem.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.AddInfrastructureRegistration();
+builder.AddServicesRegistration();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(""));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
