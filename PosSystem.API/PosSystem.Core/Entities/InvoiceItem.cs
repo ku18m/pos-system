6dba66975f042dc6bf238bab45d2dced623e0544
Mohamed Kamal <ku18mz@gmail.com>
2024-09-10 01:36:43 +0300

    Models Created Based on the ERD and SRS

---
+++ PosSystem.API/PosSystem.Core/Entities/InvoiceItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosSystem.Core.Entities
+{
+    public class InvoiceItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string InvoiceId { get; set; }
+        public virtual Invoice Invoice { get; set; }
+
+        public string ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public string UnitId { get; set; }
+        public virtual Unit Unit { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public decimal FinalAmount { get; set; }
+    }
+}
---
+++ PosSystem.API/PosSystem.Core/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosSystem.Core.Entities
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
